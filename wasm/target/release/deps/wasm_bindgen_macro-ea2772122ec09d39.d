e1e3b2c468b474415e8f2881da9d751cadcdd37b
elarsaks <elarsaks@gmail.com>
2026-01-10 18:59:17 +0700

    Add build artifacts.

---
+++ wasm/target/release/deps/wasm_bindgen_macro-ea2772122ec09d39.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/elars/Desktop/Thesis/umap-wasm/wasm/target/release/deps/wasm_bindgen_macro-ea2772122ec09d39.d: /home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-macro-0.2.106/src/lib.rs
+
+/mnt/c/Users/elars/Desktop/Thesis/umap-wasm/wasm/target/release/deps/libwasm_bindgen_macro-ea2772122ec09d39.so: /home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-macro-0.2.106/src/lib.rs
+
+/home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/wasm-bindgen-macro-0.2.106/src/lib.rs:
---
+++ wasm/target/wasm32-unknown-unknown/release/deps/unicode_ident-5387bee7b332072e.d
@@ -0,0 +1,8 @@
+/mnt/c/Users/elars/Desktop/Thesis/umap-wasm/wasm/target/wasm32-unknown-unknown/release/deps/unicode_ident-5387bee7b332072e.d: /home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
+
+/mnt/c/Users/elars/Desktop/Thesis/umap-wasm/wasm/target/wasm32-unknown-unknown/release/deps/libunicode_ident-5387bee7b332072e.rlib: /home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
+
+/mnt/c/Users/elars/Desktop/Thesis/umap-wasm/wasm/target/wasm32-unknown-unknown/release/deps/libunicode_ident-5387bee7b332072e.rmeta: /home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs /home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs
+
+/home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/lib.rs:
+/home/elars/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.22/src/tables.rs:
